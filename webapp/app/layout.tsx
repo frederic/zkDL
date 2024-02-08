6d45acd1f1f5cdd0d4d7e6a21c29403492a022ad
Frédéric Basse <contact@fredericb.info>
2024-02-07 21:43:00 -0800

    add RainbowKit

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
+import '@rainbow-me/rainbowkit/styles.css';
 
 import { cn } from "@/lib/utils"
+import { Providers } from "./providers";
  
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -24,7 +26,9 @@ export default function RootLayout({
       <body className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
-        )}>{children}</body>
+        )}>
+          <Providers>{children}</Providers>
+        </body>
     </html>
   );
 }
